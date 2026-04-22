a7ade70e9e6bf2cf5bb795c7478b21ef917876bd
Strangebrewer <borrowed.carbon.atoms@gmail.com>
2026-04-21 22:23:35 -0600

    update seeds to use mongo

--- start-go.sh
+++ start-go.sh
@@ -4,19 +4,6 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR"
 
-MIGRATE=false
-for arg in "$@"; do
-  case $arg in
-    -m|--migrate) MIGRATE=true ;;
-  esac
-done
-
 docker compose up -d --wait
-
-if [ "$MIGRATE" = true ]; then
-  (cd ../backend/go-auth && go run ./cmd/migrate up)
-  (cd ../backend/go-job-search && go run ./cmd/migrate up)
-  (cd ../backend/go-budget && go run ./cmd/migrate up)
-fi
 
 pnpm run serve
